7cdf192b1638e3bd4cf2458ef9f667f6393f099a
Vladimir Anisimov <anisim101@gmail.com>
2022-08-03 19:07:21 +0200

    no message

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,6 +7,14 @@ let tgApi: String = "5391260542:AAHwjlWVGF-1ySYe19T_rlMIlu0vc8f-x4c"
 // configures your application
 public func configure(_ app: Application) throws {
     
+//    let databaseConfig: PostgreSQLDatabaseConfig
+//    if let url = Environment.get("DATABASE_URL") {
+//        // configuring database
+//        databaseConfig = PostgreSQLDatabaseConfig(url: url, transport: .unverifiedTLS)!
+//    } else {
+//
+//    }
+    
     let databaseConfiguration = PostgresConfiguration(hostname: "localhost",
                                                       port: 5432,
                                                       username: "postgres",
